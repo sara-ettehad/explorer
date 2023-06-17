2c550b356e1172d0ff910c8e2a140c542bcb8195
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2023-06-16 23:59:52 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Card(props){
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img"/>
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  )
+}
+ReactDOM.render(
+  <div>
+    <h1>My Contacts</h1>
+
+    <Card 
+      name="Beyonce" 
+      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" 
+      tel="+123 456 789" 
+      email="b@beyonce.com"/>
+    <Card 
+      name="Jack Bauer" 
+      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg" 
+      tel="+987 654 321" 
+      email="jack@nowhere.com"/>
+    <Card 
+      name="Chuck Norris" 
+      img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png" 
+      tel="+918 372 574" 
+      email="gmail@chucknorris.com"/>
+
+    <input id="fName"/>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
